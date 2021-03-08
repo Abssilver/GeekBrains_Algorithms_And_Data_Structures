5bb2b74f26494c7f0c6a010aceaed87415a08913
Павел Ремизов <remizov@rightway.games>
2021-03-08 22:44:10 +0300

    Урок 4. Практическое задание 2. Двоичное дерево

---
+++ Homeworks/Lesson_4_2/TreeHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Lesson_4_2
+{
+    public static class TreeHelper
+    {
+        public static VertexInfo[] GetTreeInLine(ITree tree)
+        {
+            var buffer = new Queue<VertexInfo>();
+            var returnArray = new List<VertexInfo>();
+            var root = new VertexInfo { Vertex = tree.GetRoot() };
+            buffer.Enqueue(root);
+
+            while (buffer.Count != 0)
+            {
+                var element = buffer.Dequeue();
+                returnArray.Add(element);
+
+                var depth = element.Depth + 1;
+
+                if (element.Vertex.Left != null)
+                {
+                    var left = new VertexInfo
+                    {
+                        Vertex = element.Vertex.Left,
+                        Depth = depth,
+                    };
+                    buffer.Enqueue(left);
+                }
+                if (element.Vertex.Right != null)
+                {
+                    var right = new VertexInfo
+                    {
+                        Vertex = element.Vertex.Right,
+                        Depth = depth,
+                    };
+                    buffer.Enqueue(right);
+                }
+            }
+            return returnArray.ToArray();
+        }
+    }
+}
