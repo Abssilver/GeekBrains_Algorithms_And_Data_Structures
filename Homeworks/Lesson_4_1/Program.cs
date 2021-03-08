b9f1bc40d2f132a26c13d323056154956eac573c
Павел Ремизов <remizov@rightway.games>
2021-03-08 22:31:58 +0300

    Урок 4. Практическое задание 1. Сравнение поиска в array и hashset

---
+++ Homeworks/Lesson_4_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Lesson_4_1
+{
+    class Program
+    {
+        /*
+        BenchmarkDotNet=v0.12.1, OS=Windows 10.0.18363.1379 (1909/November2018Update/19H2)
+        Intel Core i7-8750H CPU 2.20GHz (Coffee Lake), 1 CPU, 12 logical and 6 physical cores
+            .NET Core SDK=5.0.103
+        [Host]     : .NET Core 5.0.3 (CoreCLR 5.0.321.7212, CoreFX 5.0.321.7212), X64 RyuJIT
+        DefaultJob : .NET Core 5.0.3 (CoreCLR 5.0.321.7212, CoreFX 5.0.321.7212), X64 RyuJIT
+
+
+        |                       Method |         Mean |      Error |     StdDev |       Median |
+        |----------------------------- |-------------:|-----------:|-----------:|-------------:|
+        |     Test_Search_ValueInArray |   892.749 us |  4.4470 us |  3.9421 us |   892.596 us |
+        |   Test_Search_ValueInHashSet |     2.971 us |  0.0212 us |  0.0198 us |     2.972 us |
+        |   Test_Search_NoValueInArray | 1,208.180 us | 23.9491 us | 56.9175 us | 1,234.391 us |
+        | Test_Search_NoValueInHashSet |    27.811 us |  0.2871 us |  0.2685 us |    27.689 us |
+        */
+
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(SearchTests).Assembly).Run(args);
+        }
+    }
+}
---
+++ Homeworks/Lesson_4_1/SearchTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace Lesson_4_1
+{
+    public class SearchTests
+    {
+        private readonly string [] _array;
+        private readonly HashSet<string> _hashSet;
+        private readonly Random _generator;
+        private readonly int _testCases;
+
+        public SearchTests()
+        {
+            _array = new string[10000];
+            _hashSet = new HashSet<string>();
+            _generator = new Random();
+            _testCases = 50;
+            GenerateTestStringData(ref _array, _hashSet);
+        }
+
+        #region GenerateData
+        private void GenerateTestStringData(ref string[] array, HashSet<string> hashSet)
+        {
+            if (array == null)
+                array = new string[10000];
+            
+            if (hashSet == null)
+                hashSet = new HashSet<string>();
+            else hashSet.Clear();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string newWord = GenerateString(_generator.Next(30, 40));
+                
+                while (hashSet.Contains(newWord))
+                {
+                    newWord = GenerateString(_generator.Next(30, 40));
+                }
+
+                hashSet.Add(newWord);
+                array[i] = newWord;
+            }
+        }
+
+        private string GenerateString(int length)
+        {
+            int firstLetter = 'a';
+            int lastLetter = 'z';
+            char[] word = new char[length];
+            for (int j = 0; j < length; j++)
+            {
+                word[j] = (char)_generator.Next(firstLetter, lastLetter + 1);
+            }
+            return new string(word);
+        }
+        
+        #endregion
+
+        #region Methods
+
+        private bool SearchInArray(string value)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i].Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool SearchInHashSet(string value) => _hashSet.Contains(value);
+
+        #endregion
+
+        #region Tests
+
+        [Benchmark]
+        public void Test_Search_ValueInArray()
+        {
+            for (int i = 0; i < _testCases; i++)
+            {
+                int randomIndex = _generator.Next(0, _array.Length);
+                SearchInArray(_array[randomIndex]);
+            }
+        }
+        
+        [Benchmark]
+        public void Test_Search_ValueInHashSet()
+        {
+            for (int i = 0; i < _testCases; i++)
+            {
+                int randomIndex = _generator.Next(0, _array.Length);
+                SearchInHashSet(_array[randomIndex]);
+            }
+        }
+
+        [Benchmark]
+        public void Test_Search_NoValueInArray()
+        {
+            for (int i = 0; i < _testCases; i++)
+            {
+                string valueIsNotPresented = GenerateString(45);
+                SearchInArray(valueIsNotPresented);
+            }
+        }
+        
+        [Benchmark]
+        public void Test_Search_NoValueInHashSet()
+        {
+            for (int i = 0; i < _testCases; i++)
+            {
+                string valueIsNotPresented = GenerateString(45);
+                SearchInHashSet(valueIsNotPresented);
+            }
+        }
+        
+        #endregion
+    }
+}
